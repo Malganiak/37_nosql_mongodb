9dfd168a747d6f35c992fc82d2db35e6d6ccf55b
Léo <malganiak@gmail.com>
2024-05-13 14:04:39 +0200

    Update create_env.cmd

--- create_env.cmd
+++ create_env.cmd
@@ -1,3 +1,3 @@
-conda create --prefix "C:\Users\Leo\Documents\Cours\Cours Simplon\Github\37_nosql_mongodb\env" python=3.10
+conda create --prefix "\Github\37_nosql_mongodb\env" python=3.10
 
-conda activate "C:\Users\Leo\Documents\Cours\Cours Simplon\Github\37_nosql_mongodb\env"
+conda activate "\Github\37_nosql_mongodb\env"
